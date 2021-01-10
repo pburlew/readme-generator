f8ae7ae42cb063f2a0e6d1e5f36b6652df4c9d2f
Paige Burlew <paigeburlew@gmail.com>
2021-01-10 14:57:10 -0600

    updated file structure

---
+++ index.js
@@ -0,0 +1,69 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+const open = require("open");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "Please provide the project licence or your badge link"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+];
+
+// function to initialize program
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    const htmlString = generateMarkdown(answers);
+    await writeToFile("README.md", htmlString, (err) => console.log(err));    
+};
+
+// function call to start program
+init();
